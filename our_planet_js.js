8b650b3e3f4d760d65a40f267d2d51669531e00c
Siddharth Chatterjee <59141222+siddharthchatterjee9@users.noreply.github.com>
2020-07-19 16:17:00 +0100

    Add files via upload

---
+++ our_planet_js.js
@@ -0,0 +1,20 @@
+< iframe style = "border: 1px solid rgba(0, 0, 0, 0.1);"
+width = "800"
+height = "450"
+src = "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FF40J7pbKap4Z9tQnO8RMpL%2FOur_Planet%3Fnode-id%3D0%253A1&chrome=DOCUMENTATION"
+allowfullscreen >
+    <
+    /iframe>
+
+const entryBox = document.querySelector(".gallery");
+const entryBox = document.querySelector(".contributions");
+const entryBox = document.querySelector(".about");
+const entryBox = document.querySelector(".conatctinfo");
+
+entryBtn.addEventListener("click", () => {
+    entryBox.classList.add("active");
+    containers.classList.add("active");
+    //containers mid left and right
+    <
+    /iframe>
+});
